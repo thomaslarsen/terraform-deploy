9eb35aa993d979234fadbf5fa36acf63ec55c10d
Thomas Larsen <toften@gmail.com>
2017-02-27 23:23:25 +0000

    Added Vault service

--- zones/mtzo/services.tf
+++ zones/mtzo/services.tf
@@ -1,15 +1,9 @@
 
-variable "saltmaster_kickstart_url" {
+variable "saltmaster_kickstart_url" { }
 
-}
+variable "saltmaster_kickstart_branch" { }
 
-variable "saltmaster_kickstart_branch" {
-  default = "master"
-}
-
-variable "thin_ami_id" {
-  default = "ami-8b8c57f8"
-}
+variable "thin_ami_id" { }
 
 variable "jump_instance_type" {
 }
@@ -321,4 +315,74 @@ output "consul_sg_id" {
 
 output "consul_fqdn" {
   value = "${module.consul_instance.fqdn}"
+}
+
+# --------------------------
+# vault box
+# --------------------------
+
+variable "vault_instance_type" { }
+
+variable "vault_hostname" {
+  default = "vault"
+}
+
+variable "vault_service" {
+  default = "vault-server"
+}
+
+variable "vault_port" {
+  default = 8200
+}
+
+module "vault_instance" {
+  source        = "../../modules/instance/thin"
+
+  ami_id        = "${var.thin_ami_id}"
+  instance_type = "${var.vault_instance_type}"
+
+  vpc_id        = "${data.terraform_remote_state.vpc.vpc_id}"
+  vdc_name      = "${var.vdc_name}"
+  key_name      = "${data.terraform_remote_state.vpc.vpc_key_name}"
+  domain        = "${data.terraform_remote_state.vpc.domain}"
+  zone_id       = "${data.terraform_remote_state.vpc.zone_id}"
+
+  subnet_list   = "${module.public_subnet.id}"
+  az_index      = "0"
+  sg_list       = [ "${data.terraform_remote_state.vpc.vpc_sg_id}", "${module.zone_sg.sg_id}" ]
+
+  service       = "${var.vault_service}"
+  role          = "${var.vault_service}"
+  hostname      = "${var.vault_hostname}"
+  appzone_name  = "${var.appzone_name}"
+  class         = "${var.class}"
+
+  saltmaster    = "${module.saltmaster_instance.fqdn}"
+}
+
+# Add ingress rules from all hosts in the VPC
+module "vault_ingress" {
+  source = "../../modules/sg/rule_sg"
+
+  sg_id = "${module.vault_instance.sg_id}"
+  protocol = "TCP"
+  from_port = "${var.vault_port}"
+  to_port = "${var.vault_port}"
+  source_sg_id = "${data.terraform_remote_state.vpc.vpc_sg_id}"
+}
+
+output "vault_private_ip" {
+  value = "${module.vault_instance.private_ip}"
+}
+
+output "vault_instance_id" {
+   value = "${module.vault_instance.instance_id}"
+}
+
+output "vault_sg_id" {
+  value = "${module.vault_instance.sg_id}"
+}
+
+output "vault_fqdn" {
+  value = "${module.vault_instance.fqdn}"
 }
